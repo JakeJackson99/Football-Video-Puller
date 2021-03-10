e81a3b96d540f5bde8931c040aeb8873a312fe49
JakeJackson99 <jakecjackson99@gmail.com>
2021-03-10 13:33:24 +0000

    Create datetime formatter for RFC 3339; produce documentation.
    Created a functioned that creates the proper datetime format required in video_list(). Also wrote documentation for various files according to Google's styling guide.

--- app/api.py
+++ app/api.py
@@ -1,7 +1,68 @@
 from app import service
+from datetime import datetime, timedelta
+
+
+def video_list(*id_values):
+    """Creates a list of video IDs that based on the given channel IDs.
+
+    Args:
+        *id_values: The ID of a channel.
+
+    Returns:
+        A list of video IDs.
+
+     Raises:
+        TypeError: An error occuring with a value that cannot be evaluated.
+    """
+    try:
+        if id_values:
+            id_list = list()
+
+            for id in id_values:
+                req = service.search().list(
+                    part='snippet',
+                    channelId=id,
+                    maxResults=2,
+                    publishedAfter=None,
+                    q='a',
+                    type='video'
+                )
+                res = req.execute()
+                for video_id in res.get('items'):
+                    id_list.append(video_id['id']['videoId'])
+
+            return id_list
+        else:
+            return None
+    except TypeError as e:
+        print(e)
+
+
+def published_after(days=1):
+    """Creates the value for 'publishedAfter' for video_list().
+
+    Args:
+        days: The number of days ago the return date should be in regard to "now", default=1.
+
+    Returns:
+        The the date 24 hours ago in RFC 3339 format.
+    """
+    date = datetime.utcnow() - timedelta(days=days)
+    return date.isoformat('T') + 'Z'
 
 
 def get_id(*username):
+    """Finds a channel's ID based on its username.
+
+    Args:
+        *username: The username of a channel.
+
+    Returns: 
+        A list of channel IDs.
+
+    Raises:
+        TypeError: An error occuring with a value that cannot be evaluated.
+    """
     try:
         if username:
             id_list = list()
@@ -18,33 +79,4 @@ def get_id(*username):
         else:
             return None
     except TypeError as e:
-        print(e)
-    except Exception as e:
-        print(e)
-
-
-def video_list(*id_values):
-    try:
-        if id_values:
-            id_list = list()
-
-            for id in id_values:
-                    req = service.search().list(
-                        part='snippet',
-                        channelId=id,
-                        maxResults=2,
-                        publishedAfter=None,
-                        q='a',
-                        type='video'
-                    )
-                    res = req.execute()
-                    for video_id in res.get('items'):
-                        id_list.append(video_id['id']['videoId'])
-
-            return id_list
-        else:
-            return None
-    except TypeError as e:
-        print(e)
-    except Exception as e:
         print(e)
--- app/routes.py
+++ app/routes.py
@@ -6,5 +6,10 @@ from app.api import video_list
 
 @app.route('/')
 def index():
+    """The index page.
+
+    Returns:
+        The index page with a list of video IDs.
+    """
     video_ids = video_list(*channel_ids)
     return render_template('index.html', video_ids=video_ids)
